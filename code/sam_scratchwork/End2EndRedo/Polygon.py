36acae237eefa2fb8fe0a190a7bb12ff220ebf64
samtenka <samtenka@umich.edu>
2016-03-24 08:21:06 -0400

    nice recursive impln. of Polygon.minus()

--- code/sam_scratchwork/End2EndRedo/Polygon.py
+++ code/sam_scratchwork/End2EndRedo/Polygon.py
@@ -27,23 +27,33 @@ class Polygon:
     def intersect(self, other):
         '''Returns set of points in both input regions, as a Polygon.'''
         return Polygon([bs.meet(bo) for bs in self.boxes for bo in other.boxes], is_disjoint=True)
-    def remove(self, other):
-        '''Shrinks `self` so as not to include points in `other`.'''
-        self.boxes.append(None) #sentinel
-        while True:
+    def minus(self, other):
+        '''Returns set of points in `self` but not in `other`, as a Polygon.
+           Implemented recursively.
+        '''
+        newboxes = []
+        while self.boxes:
             b, self.boxes = self.boxes[0], self.boxes[1:]
-            if b is None: break
-            for bo in other.boxes:
+            for j,bo in enumerate(other.boxes):
                 if not b.overlaps(bo): continue
-                self.boxes += b.minus(bo)
+                newboxes += Polygon(b.minus(bo)).minus(Polygon(other.boxes[j+1:]))
             else:
-                self.boxes.append(b)
+                newboxes.append(b)
+        return Polygon(newboxes)
+    def remove(self, other):
+        '''Shrinks `self` so as not to include any points in `other`.'''
+        self.coors = self.minus(other).coors
     def area(self, newspage=None):
         '''Counts interior pixels, potentially weighted by pixel values.
            Optional argument `newspage` is of type NewsPage.
         '''
         return sum(b.area(newspage) for b in self.boxes)
+    def __bool__(self):
+        return bool(self.area())
     def overlaps(self, other):
+        '''Checks whether a postive-area region of pixels lies in both polygons.
+           Faster than `return bool(self.intersect(other))` when much overlap.
+        '''
         for bs in self.boxes:
             for bo in other.boxes:
                 if bs.overlaps(bo): return True
