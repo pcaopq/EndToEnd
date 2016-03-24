7132a0b96fb11aa9bc7db7eb37035d4f969c14de
samtenka <samtenka@umich.edu>
2016-03-24 12:29:55 -0400

    trying to get image to scale with window...

--- code/sam_scratchwork/End2EndRedo/Article.py
+++ code/sam_scratchwork/End2EndRedo/Article.py
@@ -3,13 +3,13 @@
 
 class Article:
     '''An `Article` is a pair of Polygons, often with one element empty,
-       one representing a `title` region, another, a `text` region.
+       one representing a `title` region, another, an `article` region.
     '''
     def __init__(self, *args):
-        '''Initialize from `title_polygon,text_polygon` (title comes earlier)
+        '''Initialize from `title_polygon,article_polygon` (title comes earlier)
            or from a dictionary.'''
         if len(args)==2:
             title_polygons, text_polygons = args
-            self.polygons_by_type = {'title':title_polygon, 'text':text_polygon}
+            self.polygons_by_type = {'title':title_polygon, 'article':text_polygon}
         else:
             self.polygons_by_type = args[0]
--- code/sam_scratchwork/End2EndRedo/Visualizer.py
+++ code/sam_scratchwork/End2EndRedo/Visualizer.py
@@ -1,4 +1,6 @@
-'''@author Samuel Tenka
+'''@author Samuel Tenka.
+   Thanks to
+   http://stackoverflow.com/questions/22835289/how-to-get-tkinter-canvas-to-dynamically-resize-to-window-width
 '''
 
 from tkinter import *
@@ -29,6 +31,26 @@ def size_of_image(image_name):
     im=Image.open(image_name)
     return im.size[::-1] #[height,width]
 
+class ResizingCanvas(Canvas):
+    ''' A subclass of Canvas for dealing with resizing of windows
+        Thanks to
+        http://stackoverflow.com/questions/22835289/how-to-get-tkinter-canvas-to-dynamically-resize-to-window-width
+    '''
+    def __init__(self,parent,**kwargs):
+        Canvas.__init__(self,parent,**kwargs)
+        self.bind("<Configure>", self.on_resize)
+        self.height = self.winfo_reqheight()
+        self.width = self.winfo_reqwidth()
+
+    def on_resize(self,event):
+        hscale = float(event.height)/self.height
+        wscale = float(event.width)/self.width
+        self.height = event.height
+        self.width = event.width
+        # rescale all the objects tagged with the "all" tag
+        self.scale("all",0,0,wscale,hscale)
+        self.get(self.imageid)
+
 class Visualizer:
     def __init__(self,image_name):
         self.setup_gui()
@@ -36,19 +58,20 @@ class Visualizer:
         self.hs,self.ws = 640.0/h,480.0/w
     def setup_gui(self):
         self.master = Tk()
-        self.canvas = Canvas(self.master, height=640+20, width=480+20)
-        self.canvas.pack()
+        self.canvas = ResizingCanvas(self.master, height=640+2, width=480+2)
+        self.canvas.pack(fill=BOTH,expand=YES)
     def refresh_canvas(self):
         self.canvas.delete('all')
     def draw_box(self, box, color, content_class, activewidth=3):
         (y,x),(Y,X) = box.coors
         st,ast = ('gray75','gray50') if content_class=='title' else ('gray25','gray12')
-        self.canvas.create_rectangle(x*self.ws+10,y*self.hs+10,X*self.ws+10,Y*self.hs+10, fill=color, stipple=st, activestipple=ast, activewidth=activewidth)
+        self.canvas.create_rectangle(x*self.ws+1,y*self.hs+1,X*self.ws+1,Y*self.hs+1, fill=color, stipple=st, activestipple=ast, activewidth=activewidth)
     def display(self, segmentation):
         self.refresh_canvas()
         self.load_background('0003.jpg')
         for a,col in zip(segmentation.articles,generate_colors()):
             for content_class, p in a.polygons_by_type.items():
+                if content_class not in ('title','article'):continue
                 for b in p.boxes:
                     self.draw_box(b, col, content_class)
         mainloop()
@@ -56,7 +79,7 @@ class Visualizer:
         self.image = Image.open(image_name)
         self.image = self.image.resize((480, 640), Image.ANTIALIAS)
         self.image = ImageTk.PhotoImage(self.image)
-        self.canvas.create_image(480/2 + 10,640/2 + 10, image=self.image)
+        self.canvas.create_image(480/2 + 1,640/2 + 1, image=self.image, tag='image')
 
 from Segmentation import Segmentation
 S = Segmentation('0003.json')
